e435ffed7de8cab625d1909eb031bbf59e34f4dc
Geevarghese Regi <geevargheseregi78@gmail.com>
2023-06-24 19:30:40 -0700

    added git ignore and first expt done

--- non_recursive_factorial.c
+++ non_recursive_factorial.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int fact(int x);
+
 void main()
 {
-    int a;
+    int n, factorial;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    factorial = fact(n);
+    printf("The factorial of %d = %d", n, factorial);
+}
+
+int fact(x)
+{
+    int i, factorial = 1;
+    for (i = 1; i <= x; i++)
+    {
+        factorial = factorial * i;
+    }
+    return factorial;
 }
